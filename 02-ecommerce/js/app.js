11a7902396243726db22b6b603aad5fef66a56ae
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-01-01 08:04:59 +0100

    TV- Validación de Formularios en Bootstrap

--- 02-ecommerce/js/app.js
+++ 02-ecommerce/js/app.js
@@ -7,26 +7,54 @@
 // show.bs.modal
 const modalImagen = document.querySelector('#modal-imagen');
 
-modalImagen.addEventListener('show.bs.modal', function (event) {
-  const enlace = event.relatedTarget;
-  const rutaImagen = enlace.getAttribute('data-bs-imagen');
+if (modalImagen) {
+  modalImagen.addEventListener('show.bs.modal', function (event) {
+    const enlace = event.relatedTarget;
+    const rutaImagen = enlace.getAttribute('data-bs-imagen');
 
-  // Construir la imagen
-  // Los elementos usualmente se van a crear con la etiqueta en mayúsculas
-  const imagen = document.createElement('IMG');
-  imagen.src = `img/${rutaImagen}.jpg`;
-  imagen.classList.add('img-fluid');
-  imagen.alt = 'Imagen Galería';
+    // Construir la imagen
+    // Los elementos usualmente se van a crear con la etiqueta en mayúsculas
+    const imagen = document.createElement('IMG');
+    imagen.src = `img/${rutaImagen}.jpg`;
+    imagen.classList.add('img-fluid');
+    imagen.alt = 'Imagen Galería';
 
-  // Quiero mostrar la imagen en la clase modal-body
-  const contenidoModal = document.querySelector('.modal-body');
-  contenidoModal.appendChild(imagen);
+    // Quiero mostrar la imagen en la clase modal-body
+    const contenidoModal = document.querySelector('.modal-body');
+    contenidoModal.appendChild(imagen);
 
-  // console.log(imagen);
-});
+    // console.log(imagen);
+  });
 
-// Cuando se cierra el modal quito la imagen del Modal
-modalImagen.addEventListener('hidden.bs.modal', function () {
-  const contenidoModal = document.querySelector('.modal-body');
-  contenidoModal.textContent = '';
-});
+  // Cuando se cierra el modal quito la imagen del Modal
+  modalImagen.addEventListener('hidden.bs.modal', function () {
+    const contenidoModal = document.querySelector('.modal-body');
+    contenidoModal.textContent = '';
+  });
+}
+
+// Necesario para usar con la clase needs-validation
+// Fuente cogido de: https://getbootstrap.com/docs/5.3/forms/validation/#how-it-works
+// Este código cambia muchas veces. Ir a la página y coger lo que tengan ahí.
+(() => {
+  'use strict';
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      'submit',
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      },
+      false
+    );
+  });
+})();
